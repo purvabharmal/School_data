587fdd5ba8f552837ba66da0b26646364e26e8e6
purvabharmal <purvabharmal1@gmail.com>
2026-02-19 22:54:24 +0530

    first challenge

--- Static_var.java
+++ Static_var.java
@@ -8,24 +8,37 @@ class Mobile
     {
         System.out.println(brand+" : "+price+" : "+name);
     }
+
+    static{
+        System.out.println("in static block");
+    }
+
+    public static void show1(Mobile obj)
+    {
+        System.out.println(obj.brand+" : "+obj.price+" : "+name);
+        System.out.println("in static method show1");
+    }
 }
 
 public class Static_var {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
 
-        Mobile obj1=new Mobile();
-        obj1.brand="Apple";
-        obj1.price=150000;
-        Mobile.name="Smartphone";
+        Class.forName("Mobile");
+        // Mobile obj1=new Mobile();
+        // obj1.brand="Apple";
+        // obj1.price=150000;
+        // Mobile.name="Smartphone";
 
-        Mobile obj2=new Mobile();
-        obj2.brand="Samsung";
-        obj2.price=100000;
-        Mobile.name="phone";
+        // Mobile obj2=new Mobile();
+        // obj2.brand="Samsung";
+        // obj2.price=100000;
+        // Mobile.name="phone";
 
-        obj1.show();
-        obj2.show();
+        // obj1.show();
+        // obj2.show();
+
+        // Mobile.show1(obj1);
 
     }
     
